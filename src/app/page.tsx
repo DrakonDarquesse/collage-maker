bab19fc026da56e0ea80b184f1762322b007f9cf
Ming Liang Tay <mingliangzheng17@gmail.com>
2024-05-04 17:23:23 +0800

    build(deps): next + typescript + tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between align-center p-24">
+      <Link className="px-2 py-3 bg-white text-black rounded" href="/fly">
+        Create Collage
+      </Link>
+    </main>
+  );
+}
