eef80154e95def5c38000fca35ac064f810a1174
Ming Liang Tay <mingliang@bim.local>
2025-04-11 17:25:37 +0800

    fix: link direct to /collage

---
+++ src/app/collage/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Collage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between align-center p-24">
+      in
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between align-center p-24">
-      <Link className="px-2 py-3 bg-white text-black rounded" href="/fly">
+      <Link className="px-2 py-3 bg-white text-black rounded" href="/collage">
         Create Collage
       </Link>
     </main>
